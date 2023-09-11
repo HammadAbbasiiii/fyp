b66ba918207007bbfd76fc3bbd7936fade673383
Hammad Abbasi <143787522+HammadAbbasiiii@users.noreply.github.com>
2023-09-11 02:15:18 -0700

    Update app.py

--- ml_web_app-2/app.py
+++ ml_web_app-2/app.py
@@ -20,22 +20,22 @@ lsvc = joblib.load('linear_svc_model.pkl')
 lstm_model = load_model('lstm_model.h5')
 ensemble_model = load_model('ensemble_model.h5')
 
-# with open('tokenizer.json', 'r', encoding='utf-8') as f:
-#     tokenizer_config = f.read()
-#     tokenizer = tokenizer_from_json(tokenizer_config)
+with open('tokenizer.json', 'r', encoding='utf-8') as f:
+    tokenizer_config = f.read()
+    tokenizer = tokenizer_from_json(tokenizer_config)
 
-# with open('maxlen.txt', 'r') as f:
-#     maxlen = int(f.read())
+with open('maxlen.txt', 'r') as f:
+    maxlen = int(f.read())
 
-# Function for stemming (You can use it when you uncomment the code above)
+Function for stemming (You can use it when you uncomment the code above)
 
-# def stemming(content):
-#     con = re.sub('[^a-zA-Z]', ' ', content)
-#     con = con.lower()
-#     con = con.split()
-#     con = [port_stem.stem(word) for word in con if not word in stopwords.words('english')]
-#     con = ' '.join(con)
-#     return con
+def stemming(content):
+    con = re.sub('[^a-zA-Z]', ' ', content)
+    con = con.lower()
+    con = con.split()
+    con = [port_stem.stem(word) for word in con if not word in stopwords.words('english')]
+    con = ' '.join(con)
+    return con
 @app.route('/')
 def index():
     return render_template('index.html')
