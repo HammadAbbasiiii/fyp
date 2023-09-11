0706fc40b28b3aca0189de5533188adb2a83eb07
Hammad Abbasi <143787522+HammadAbbasiiii@users.noreply.github.com>
2023-09-11 03:06:23 -0700

    Update app.py

--- ml_web_app-2/app.py
+++ ml_web_app-2/app.py
@@ -4,8 +4,8 @@ import numpy as np
 from keras.utils import pad_sequences
 from keras.preprocessing.text import tokenizer_from_json
 from keras.models import load_model
-# from nltk.corpus import stopwords
-# from nltk.stem import PorterStemmer
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from flask import Flask, request, jsonify, render_template
 
